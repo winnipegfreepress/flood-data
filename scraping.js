03f084524689682d8d4cbe687fa710d3bd95c312
Bailey <ebailey@wfpmn056.freepress.mb.ca>
2017-03-16 13:41:51 -0500

    package and scrapper changes to use aws

--- scraping.js
+++ scraping.js
@@ -4,11 +4,26 @@ var path = require('path');
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
+var S3Config = require('s3-append').S3Config;
 
+// setup S3 config
+
+var config = new S3Config({
+  "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+  "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+  "region": process.env.S3_REGION,
+  "bucket": process.env.S3_BUCKET_NAME
+});
+
+// Using config from above 
+var S3Append = require('s3-append').S3Append;
+var service = new S3Append(config, 'data/water_levels.csv', Format.Text);
+ 
 
 //local server stuff
 var app = express();
 var port = 8000;
+
 
 //empty array for results
 var data = [];
@@ -38,12 +53,15 @@ request(url, function(err, resp, body) {
   //concantenate level, time
   data = [leveltext, timetext, '\r\n'];
 
-  fs.appendFile(outputFile, data, 'utf8', function (err) {
-  if (err) {
-    console.log('Some error occured - file either not saved or corrupted file saved.');
-  } else {
-    console.log('It is saved!');
-  }
+  service.append(data);
+ 
+  service.flush()
+  .then(function() {
+    console.log("Done!");
+  })
+  .catch(function(err) {
+    console.error(err.message);
+  });
 });
 
   //push to empty array
